707efc919a94bd21266bff1f2bbee17730bd2787
rg-ci <ci@realglobe.jp>
2017-06-13 01:43:42 +0000

    [ci skip] Travis CI committed after build

---
+++ test/runnable_test.js
@@ -0,0 +1,27 @@
+/**
+ * Test case for runnable.
+ * Runs with mocha.
+ */
+'use strict'
+
+const runnable = require('../lib/runnable.js')
+const assert = require('assert')
+const co = require('co')
+
+describe('runnable', function () {
+  this.timeout(3000)
+
+  before(() => co(function * () {
+
+  }))
+
+  after(() => co(function * () {
+
+  }))
+
+  it('Runnable', () => co(function * () {
+
+  }))
+})
+
+/* global describe, before, after, it */
